161f525abb05adba3f582322d987b103ea69f4bb
Shocewin <Shocewin@yandex.ru>
2018-10-25 05:24:19 +0300

    product initial migration

--- Startup.cs
+++ Startup.cs
@@ -23,7 +23,7 @@ namespace TeslaGame
 
         public void ConfigureServices(IServiceCollection services)
         {
-			services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["Data:SportsStoreProducts:ConnectionString"]));
+			services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["Data:TeslaGameProducts:ConnectionString"]));
 			services.AddTransient<IProductRepository, EFProductRepository>();
 			services.AddMvc();
         }
