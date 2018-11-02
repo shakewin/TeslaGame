440f1ec3b618c4633295ed791953bf29dbff7d03
Shocewin <Shocewin@yandex.ru>
2018-11-02 12:26:41 +0300

    improve urls

--- Startup.cs
+++ Startup.cs
@@ -36,6 +36,10 @@ namespace TeslaGame
 			app.UseMvc(routes =>
 			{
 				routes.MapRoute(
+					name: "pagination",
+					template: "products/page{productPage}",
+					defaults: new { Controller = "Product", action = "List" });
+				routes.MapRoute(
 					name: "default",
 					template: "{controller=Product}/{action=List}/{id?}");
 			});
