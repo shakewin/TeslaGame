6cf842f020a6494c752794d0ec3b18773d098ae0
Shocewin <Shocewin@yandex.ru>
2018-11-02 12:17:44 +0300

    register tag helper and add product list view model

---
+++ Models/ViewModels/ProductListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using TeslaGame.Models;
+
+namespace TeslaGame.Models.ViewModels
+{
+    public class ProductListViewModel
+    {
+		public IEnumerable<Product> Products { get; set; }
+		public PagingInfo PagingInfo { get; set; }
+    }
+}
