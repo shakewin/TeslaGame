ec5111e827e14de9a65d1bd631258ca2b25f0c66
Shocewin <Shocewin@yandex.ru>
2018-11-05 16:42:59 +0300

    style active state nav menu

--- Components/NavigationMenuViewComponent.cs
+++ Components/NavigationMenuViewComponent.cs
@@ -15,6 +15,7 @@ namespace TeslaGame.Components
 
 		public IViewComponentResult Invoke()
 		{
+			ViewBag.SelectedCategory = RouteData?.Values["category"];
 			return View(repository.Products
 							.Select(p => p.Category)
 							.Distinct()
