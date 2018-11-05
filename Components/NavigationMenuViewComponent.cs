cdb3fddae6a9c5aab2bc2f1f21fa995946eadefd
Shocewin <Shocewin@yandex.ru>
2018-11-05 16:21:20 +0300

    create navigation menu

---
+++ Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using TeslaGame.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace TeslaGame.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+		IProductRepository repository;
+
+		public NavigationMenuViewComponent(IProductRepository repo)
+		{
+			repository = repo;
+		}
+
+		public IViewComponentResult Invoke()
+		{
+			return View(repository.Products
+							.Select(p => p.Category)
+							.Distinct()
+							.OrderBy(x => x));
+		}
+	}
+}
