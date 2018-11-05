1a5e89485495df658a5c8826bd3648935f59e186
Shocewin <Shocewin@yandex.ru>
2018-11-05 17:26:09 +0300

    add cart

---
+++ Models/Cart.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TeslaGame.Models
+{
+    public class Cart
+    {
+		public List<CartLine> lineCollection = new List<CartLine>();
+
+		public virtual void AddItem(Product product, int quantity)
+		{
+			CartLine line = lineCollection.Where(p => p.Product.ProductID == product.ProductID).FirstOrDefault();
+			if (line == null)
+			{
+				lineCollection.Add(new CartLine { Product = product, Quantity = quantity });
+			}
+			else line.Quantity += quantity;
+		}
+
+		public virtual void RemoveLive(Product product) =>
+			lineCollection.RemoveAll(l => l.Product.ProductID == product.ProductID);
+
+		public virtual void ComputeTotalValue(Product product) =>
+			lineCollection.Sum(p => p.Product.Price * p.Quantity);
+
+		public virtual void Clear() => lineCollection.Clear();
+
+		public virtual IEnumerable<CartLine> Lines => lineCollection;
+	}
+
+	public class CartLine
+	{
+		public int CardID { get; set; }
+		public Product Product { get; set; }
+		public int Quantity { get; set; }
+	}
+}
