7a528b2eca89e3614d30d2386ae5ad369627eedf
Shocewin <Shocewin@yandex.ru>
2018-11-05 17:31:28 +0300

    fix url extension

--- Infrastructure/UrlExtensions.cs
+++ Infrastructure/UrlExtensions.cs
@@ -2,7 +2,7 @@
 
 namespace TeslaGame.Infrastructure
 {
-    public class UrlExtensions
+    public static class UrlExtensions
     {
 		public static string PathAndQuery(this HttpRequest request) =>
 			request.QueryString.HasValue ? $"{request.Path}{request.QueryString}" : request.Path.ToString();
