1889b576fcc3a2f4c8f2b16681aa58568d70b5c3
Shocewin <Shocewin@yandex.ru>
2018-11-05 19:39:46 +0300

    fix bag from cart

--- Models/Cart.cs
+++ Models/Cart.cs
@@ -3,34 +3,36 @@ using System.Linq;
 
 namespace TeslaGame.Models
 {
-    public class Cart
-    {
-		public List<CartLine> lineCollection = new List<CartLine>();
-
+	public class Cart
+	{
+		List<CartLine> lineCollection = new List<CartLine>();
 		public virtual void AddItem(Product product, int quantity)
 		{
-			CartLine line = lineCollection.FirstOrDefault(p => p.Product.ProductID == product.ProductID);
+			CartLine line = lineCollection
+			  .FirstOrDefault(p => p.Product.ProductID == product.ProductID);
 			if (line == null)
 			{
-				lineCollection.Add(new CartLine { Product = product, Quantity = quantity });
+				lineCollection.Add(new CartLine
+				{
+					Product = product,
+					Quantity = quantity
+				});
 			}
-			else line.Quantity += quantity;
+			else
+			{
+				line.Quantity += quantity;
+			}
 		}
-
 		public virtual void RemoveLine(Product product) =>
-			lineCollection.RemoveAll(l => l.Product.ProductID == product.ProductID);
-
+		 lineCollection.RemoveAll(l => l.Product.ProductID == product.ProductID);
 		public virtual decimal ComputeTotalValue() =>
-			lineCollection.Sum(p => p.Product.Price * p.Quantity);
-
+		 lineCollection.Sum(e => e.Product.Price * e.Quantity);
 		public virtual void Clear() => lineCollection.Clear();
-
 		public virtual IEnumerable<CartLine> Lines => lineCollection;
 	}
-
 	public class CartLine
 	{
-		public int CardID { get; set; }
+		public int CartLineID { get; set; }
 		public Product Product { get; set; }
 		public int Quantity { get; set; }
 	}
