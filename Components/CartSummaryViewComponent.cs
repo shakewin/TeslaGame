a989404a714769872082519175b87ee630909334
Shocewin <Shocewin@yandex.ru>
2018-11-07 13:26:49 +0300

    add cart summary view component

---
+++ Components/CartSummaryViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TeslaGame.Models;
+
+namespace TeslaGame.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+		private Cart cart;
+
+		public CartSummaryViewComponent(Cart cartService)
+		{
+			cart = cartService;
+		}
+
+		public IViewComponentResult Invoke() => View(cart);
+	}
+}
