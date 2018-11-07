e509e6cd6fc35cd9998ee5d3bbe1f4b096e16124
Shocewin <Shocewin@yandex.ru>
2018-11-07 14:42:26 +0300

    add checkout validation and completed view

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using TeslaGame.Models;
 
@@ -6,6 +6,37 @@ namespace TeslaGame.Controllers
 {
     public class OrderController : Controller
     {
+		private IOrderRepository repository;
+		private Cart cart;
+
+		public OrderController(IOrderRepository repo, Cart cartService)
+		{
+			repository = repo;
+			cart = cartService;
+		}
+
 		public ViewResult Checkout() => View(new Order());
-    }
+
+		[HttpPost]
+		public IActionResult Checkout(Order order)
+		{
+			if (!cart.Lines.Any())
+			{
+				ModelState.AddModelError("", "Ваша корзина пуста!");
+			}
+			if (ModelState.IsValid)
+			{
+				order.Lines = cart.Lines.ToArray();
+				repository.SaveOrder(order);
+				return RedirectToAction(nameof(Completed));
+			}
+			return View(order);
+		}
+
+		public ViewResult Completed()
+		{
+			cart.Clear();
+			return View();
+		}
+	}
 }
