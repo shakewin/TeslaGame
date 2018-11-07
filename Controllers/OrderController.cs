9968408a5448320766016f81bf9ae88a8fb82fdb
Shocewin <Shocewin@yandex.ru>
2018-11-07 13:48:31 +0300

    create order controller

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TeslaGame.Models;
+
+namespace TeslaGame.Controllers
+{
+    public class OrderController : Controller
+    {
+		public ViewResult Checkout() => View(new Order());
+    }
+}
