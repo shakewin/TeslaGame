ea9c876e2641193789c0a6212d47b65aa942c21c
Shocewin <Shocewin@yandex.ru>
2018-11-07 13:36:43 +0300

    add order model

---
+++ Models/Order.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TeslaGame.Models
+{
+	public class Order
+	{
+		[BindNever]
+		public int OrderID { get; set; }
+		[BindNever]
+		public ICollection<CartLine> Lines { get; set; }
+
+		[Required(ErrorMessage = "Введите свой идентификатор")]
+		public string Nickname { get; set; }
+
+		[Required(ErrorMessage = "Введите свою страну")]
+		public string Country { get; set; }
+
+		[Required(ErrorMessage = "Ввeдите свой e-mail")]
+		public string Mail { get; set; }
+
+		[Required(ErrorMessage = "Выберите способ оплаты")]
+		public string Pay { get; set; }
+
+	}
+}
