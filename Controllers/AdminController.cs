463ab8b078767a84a5e9dbddf01f1b984eb7d823
Shocewin <Shocewin@yandex.ru>
2018-11-08 21:24:27 +0300

    add admin controller

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TeslaGame.Models;
+
+namespace TeslaGame.Controllers
+{
+	public class AdminController : Controller
+	{
+		private IProductRepository repository;
+		public AdminController(IProductRepository repo)
+		{
+			repository = repo;
+		}
+
+		public ViewResult Index() => View(repository.Products);
+	}
+}
