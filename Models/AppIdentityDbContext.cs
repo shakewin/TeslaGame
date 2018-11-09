b917081b7e0941602355632c4cab6fbbcf95b061
Shocewin <Shocewin@yandex.ru>
2018-11-09 17:31:58 +0300

    create identity db context

---
+++ Models/AppIdentityDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace TeslaGame.Models
+{
+    public class AppIdentityDbContext : IdentityDbContext<IdentityUser>
+	{
+		public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options)
+		{
+		}
+	}
+
+	public class MyContextFactory : IDesignTimeDbContextFactory<AppIdentityDbContext>
+	{
+		public AppIdentityDbContext CreateDbContext(string[] args)
+		{
+			var builder = new DbContextOptionsBuilder<AppIdentityDbContext>();
+			builder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=TeslaIdentity;Trusted_Connection=True;MultipleActiveResultSets=true");
+			return new AppIdentityDbContext(builder.Options);
+		}
+	}
+}
